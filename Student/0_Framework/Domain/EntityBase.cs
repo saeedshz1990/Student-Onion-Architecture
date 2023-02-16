7551329ade578e644b5ccd2f35efd16c2399e7bb
Saeed Ansari <saeed.ansari1990@hotmail.com>
2023-02-16 22:36:49 +0330

    implementiong infrastructure of project

---
+++ Student/0_Framework/Domain/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace _0_Framework.Domain
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ Student/0_Framework/Domain/PersonEntityBase.cs
@@ -0,0 +1,13 @@
+﻿namespace _0_Framework.Domain
+{
+    public class PersonEntityBase
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string MobilePhone { get; set; } = string.Empty;
+        public string NationalNumber { get; set; } = string.Empty;
+        public int YearBirth { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
